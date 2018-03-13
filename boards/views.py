d181ee979a436bb70bea45a39ba1af3882b5b53d
wagagt <wagagt@gmail.com>
2018-03-12 22:25:08 -0600

    Part IV: form new topic.

--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, Http404
-
-
-from .models import Board
+from django.contrib.auth.models import User
+from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 
 def home(request):
@@ -12,3 +12,24 @@ def home(request):
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board':board})
+
+def new_topic (request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first() # TODO: get authenticated user
+    if request.method =='POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board':board, 'form':form})
+
